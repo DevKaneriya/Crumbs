86c4d0360788d8f1fc949743e52dd602133614e7
DevKaneriya <devkaneriya1617@gmail.com>
2026-06-23 22:51:14 +0530

    added apps

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework',
     'corsheaders',
+    
+    'accounts',
+    'catalog',
+    'blogs',
+    
 ]
 
 
